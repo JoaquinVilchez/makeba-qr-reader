ed9e2a52fb61520298dd95612afd588ec203dbdd
Joaquin Vilchez <joaquinvilchez95@gmail.com>
2024-04-09 15:44:02 -0300

    UPDATE: Optmized code

--- src/pages/scan.jsx
+++ src/pages/scan.jsx
@@ -2,7 +2,7 @@ import { db } from "@/config/firebase";
 import { NoSSR } from "@kwooshung/react-no-ssr";
 import { Scanner } from "@yudiel/react-qr-scanner";
 import { collection, doc, getDocs, updateDoc } from "firebase/firestore";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import Swal from 'sweetalert2'
 import withReactContent from 'sweetalert2-react-content'
 
@@ -40,24 +40,21 @@ export default function Home() {
     }
   }
 
-  const scanTicket = async (text, result) => {
-    console.log(isScanning)
-    if (isScanning===false) {
-      const ticket = tickets.find(ticket => ticket.code === text);
-      if (ticket) {
-        if (ticket.status !== 'USED') {
-          await updateTicket({ status: 'USED' }, ticket.id);
-        } else {
-          modalAlert('QR INVALIDO', 'El QR ya fue escaneado', 'error', false);
-        }
-      } else {
-        modalAlert('EL QR NO EXISTE', 'El ticket no existe en la base de datos', 'error', false);
-      }
+  const scan = useCallback(async (text) => {
+    if (isScanning) return;
+    const ticket = tickets.find(({ code }) => code === text);
+    if (!ticket) {
+      return modalAlert('EL QR NO EXISTE', 'El ticket no existe en la base de datos', 'error', false);
+    }
+    if (ticket.status !== 'USED') {
+      await updateTicket({ status: 'USED' }, ticket.id);
+    } else {
+      modalAlert('QR INVALIDO', 'El QR ya fue escaneado', 'error', false);
     }
     setIsScanning(true);
-  }
+  }, [isScanning, tickets]);
 
-  const modalAlert = async (title, text, icon, isScanning) => {
+  const modalAlert = useCallback(async (title, text, icon, isScanning) => {
     Swal.fire({
       title,
       text,
@@ -67,8 +64,8 @@ export default function Home() {
       if (result.isConfirmed) {
         setIsScanning(isScanning);
       }
-    })
-  }
+    });
+  }, []);
 
   useEffect(() => {
     getTickets()
@@ -79,7 +76,7 @@ export default function Home() {
       <div className="container mx-auto mt-8 max-w-[560px]">
       <NoSSR>
         <Scanner
-          onResult={(text, result) => scanTicket(text, result)}
+          onResult={(text, result) => scan(text)}
           onError={(error) => onError(error?.message)}
           tracker={false}
         />
